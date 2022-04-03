bf3c0f107796a05c4986a7bc645898bf573a20f6
tristanw <wellmanboy06@gmail.com>
2022-04-03 14:37:35 -0400

    added functions

---
+++ interp.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+
+void file() {
+
+	std::cout << ":: Enter file name" << std::endl;
+	
+	char fname[128];
+	scanf("%127s", fname);
+
+	std::string line;
+
+	std::ifstream file(fname);
+
+	std::ofstream output("a.cpp");
+	output << "#include <wellang/well.hpp>\n";
+
+	while(getline(file, line)) {
+		int32_t FUNC_NUM = 0;
+
+		if(line.find("func:main") != std::string::npos) {
+			output << "int main() {\n";
+			if(line.find("}") != std::string::npos) {
+				output << "}\n";
+			} else {
+				output << "//" << line << "\n";
+			}
+		} else if(line.find("func:") != std::string::npos) {
+			output << "void \n";
+			int32_t num = 5;
+			while(num != line.length()) {
+				output << line[num];
+				num++;
+				
+			}
+			output << "\n";
+                        if(line.find("}") != std::string::npos) {
+                                output << "}\n";
+                        } else {
+                                output << "//" << line << "\n";
+                        }
+		} else {
+			output << line << "\n";
+		}
+		FUNC_NUM++;
+
+	}
+	
+	//output << "}\n";
+
+	file.close();
+	output.close();
+
+}
+
+void comp() {
+
+	system("g++ a.cpp -o a.o");
+	//system("rm -f a.cpp");
+	std::cout << "done compiling program" << std::endl;
+
+}
+
+int main() {
+
+	file();
+	comp();
+	return 0;
+
+}
