0e1e325b9009a1e274ec8dca1bccc3ec51dcacbc
__Oblivion__ <88514898+TristanWellman@users.noreply.github.com>
2022-05-14 18:27:06 -0400

    Delete src directory

--- src/mov_search.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#ifndef MOV_SEARCH_H
-#define MOV_SEARCH_H
-
-int mov_interp(char line[], FILE *out);
-
-#endif
--- src/push_search.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-#ifndef PUSH_SEARCH_H
-#define PUSH_SEARCH_H
-
-int push_interp(char line[], FILE *out);
-
-#endif
