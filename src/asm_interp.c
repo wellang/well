e3a19d5c9285e747270873c0ec818238e9044e4d
TristanWellman <wellmanboy06@gmail.com>
2022-12-23 11:37:26 -0500

    add: -h --help arg

--- src/asm_interp.c
+++ src/asm_interp.c
@@ -193,203 +193,202 @@ int asm_interp(int argc, char *argv[], bool INFO_DEBUG) {
 	int lineline_num = 0;
 	while(fgets(line4, sizeof(line4), file3) != NULL) {
 
-		comment_check = __check_com__(line4);
-		if(comment_check == true) {
-			  continue;
-		}
+	  comment_check = __check_com__(line4);
+	  if(comment_check == true) {
+	    continue;
+	  }
 
-		lineline_num++;
-		char search[] = "~func:";
-		char *func = strstr(line4, search);
-		char search_main[] = "~func:main"; 
-		char *main = strstr(line4, search_main);
+	  lineline_num++;
+	  char search[] = "~func:";
+	  char *func = strstr(line4, search);
+	  char search_main[] = "~func:main"; 
+	  char *main = strstr(line4, search_main);
 
-		if(func != NULL && main == NULL) {
-				if(INFO_DEBUG == true){wlog_info(fname, lineline_num, "found function at line: %d", lineline_num);}
-				char *after_func = strchr(line4, ':');
-				if(after_func != NULL) {
-						after_func++;
+	  if(func != NULL && main == NULL) {
+	    if(INFO_DEBUG == true){
+	      wlog_info(fname, lineline_num, "found function at line: %d", lineline_num);
+	    }
+	    char *after_func = strchr(line4, ':');
+	    if(after_func != NULL) {
+	      after_func++;
 						
-						char search[] = "{";
-						char *brack = strstr(after_func, search);
-						if(brack != NULL) {
-							char *function_name = strtok(after_func, "{");
-							FILE *func = fopen("a.asm", "a");
-							function_name[strlen(function_name)-1] = '\0';
-							fprintf(func, "\n%s:\n", function_name);
-							fclose(func);
+	      char search[] = "{";
+	      char *brack = strstr(after_func, search);
+	      if(brack != NULL) {
+		char *function_name = strtok(after_func, "{");
+		FILE *func = fopen("a.asm", "a");
+		function_name[strlen(function_name)-1] = '\0';
+		fprintf(func, "\n%s:\n", function_name);
+		fclose(func);
 
-							FILE *read = fopen(fname, "r+");
-							char line6[256];
-							int line_better = 0;
-							int line_line = lineline_num;
+		FILE *read = fopen(fname, "r+");
+		char line6[256];
+		int line_better = 0;
+		int line_line = lineline_num;
 
-							int ifnum = 0;
-							bool is_in_if = false;
-							int ifnum_ln = 0;
+	        int ifnum = 0;
+		bool is_in_if = false;
+		int ifnum_ln = 0;
 
-							int callnum = 0;
+		int callnum = 0;
 
-							while(fgets(line6, sizeof(line6), read) != NULL) {
-								/*char search_for_func[] = function_name;
-								char func = strstr(line6, search_for_func);
-								if(func != NULL) {
-									
-								}*/
-								comment_check = __check_com__(line6);
-								if(comment_check == true) {
-									continue;
-								}
-								if(line_better != line_line) {
-									line_better++;
-								} else if(line_better == line_line) {
-									line_better++;
-									line_line++;
-									char *end = strstr(line6, "}");
-									char *main = strstr(line6, "func:main");
-									if(end != NULL) {
-										if(INFO_DEBUG == true){wlog_info(fname, line_better, "line:%d is the end of a function\n", line_better);}
-										break;
-									} else if(main != NULL) {
-										if(INFO_DEBUG == true){log_info(fname, line_better, "line:%d found main function\n", line_better);}
-										break;
-									} else if(end == NULL && main == NULL){
-										FILE *out;
-										char *iffind = strstr(line6, "~if");
-										if(iffind != NULL) {
-											ifnum++;
-											ifnum_ln = line_better;
-											FILE *if_ = fopen(fname, "r+");
-											is_in_if = IS_IN_IF(if_, line_better, fname);
-										}
-										char *callfind = strstr(line6, "call~");
-										if(callfind != NULL) {callnum++;}
-										asm_interp_func_funcs(line6,
-															  out,
-															  line_better,
-															  ifnum_ln,
-															  fname,
-															  function_name,
-															  ifnum,
-															  callnum,
-															  is_in_if);
-									}
-								}
-							}	
-						} else if(brack == NULL) {
-							wlog_warn(fname, lineline_num, "sub function brackets not implimented yet\n\n");
-						}
+		while(fgets(line6, sizeof(line6), read) != NULL) {
+		/*char search_for_func[] = function_name;
+		char func = strstr(line6, search_for_func);
+		if(func != NULL) {
+			
+		}*/
+		  comment_check = __check_com__(line6);
+		  if(comment_check == true) {
+		    continue;
+		  }
+		  if(line_better != line_line) {
+		    line_better++;
+		  } else if(line_better == line_line) {
+		    line_better++;
+		    line_line++;
+		    char *end = strstr(line6, "}");
+		    char *main = strstr(line6, "func:main");
+		    if(end != NULL) {
+		      if(INFO_DEBUG == true){wlog_info(fname, line_better, "line:%d is the end of a function\n", line_better);}
+		      break;
+		    } else if(main != NULL) {
+		      if(INFO_DEBUG == true){log_info(fname, line_better, "line:%d found main function\n", line_better);}
+		      break;
+		    } else if(end == NULL && main == NULL){
+		      FILE *out;
+		      char *iffind = strstr(line6, "~if");
+		      if(iffind != NULL) {
+			ifnum++;
+			ifnum_ln = line_better;
+			FILE *if_ = fopen(fname, "r+");
+			is_in_if = IS_IN_IF(if_, line_better, fname);
+		      }
+		      char *callfind = strstr(line6, "call~");
+		      if(callfind != NULL) {callnum++;}
+		      asm_interp_func_funcs(line6,								                            out,
+					    line_better,
+					    ifnum_ln,
+					    fname,
+					    function_name,
+					    ifnum,
+					    callnum,
+					    is_in_if);
+		    }
+		  }
+		}	
+	      } else if(brack == NULL) {
+		wlog_warn(fname, lineline_num, "sub function brackets not implimented yet\n\n");
+	      }
 
-				} else if(after_func == NULL) {
-						wlog_error(fname, lineline_num, "Function does not have delimiter ':'\n");
-						continue;
-				}
-		} else if(func != NULL && main != NULL) {
-			if(INFO_DEBUG == true){wlog_info(fname, lineline_num, "found main fuction");}
-      			FILE *outputfunc = fopen("a.asm", "a");
+	    } else if(after_func == NULL) {
+	      wlog_error(fname, lineline_num, "Function does not have delimiter ':'\n");
+	      continue;
+	    }
+	  } else if(func != NULL && main != NULL) {
+	    if(INFO_DEBUG == true){wlog_info(fname, lineline_num, "found main fuction");}
+	    FILE *outputfunc = fopen("a.asm", "a");
 #if defined __APPLE__
-                  fprintf(outputfunc, "\n\nglobal _main\n\n_main:\n\t");
-                  fclose(outputfunc);
+            fprintf(outputfunc, "\n\nglobal _main\n\n_main:\n\t");
+	    fclose(outputfunc);
 #else
-	                fprintf(outputfunc, "\n\nglobal main\n\nmain:\n\t");
- 	                fclose(outputfunc);
+	    fprintf(outputfunc, "\n\nglobal main\n\nmain:\n\t");
+ 	    fclose(outputfunc);
 #endif
-			FILE *main_func = fopen(fname, "r+");
-			char mainline[256];
-			int mainline_num = 0;
-			int line_line = line_num;
-			while(fgets(mainline, sizeof(mainline), main_func) != NULL) {
-				comment_check = __check_com__(mainline);
-				if(comment_check == true) {
-					continue;
-				}
-				if(mainline_num != line_line) {
-					mainline_num++;
-				} else if(mainline_num == line_line) {
-					line_line++;
-					mainline_num++;
-					char *main_search = strstr(mainline, "func:main");
-					if(main_search != NULL) {
-						int the_line = mainline_num;
-						int line_yo = 0;
-						FILE *main_run = fopen(fname, "r+");
-						char lineline[256];
+	    FILE *main_func = fopen(fname, "r+");
+	    char mainline[256];
+	    int mainline_num = 0;
+	    int line_line = line_num;
+	    while(fgets(mainline, sizeof(mainline), main_func) != NULL) {
+	      comment_check = __check_com__(mainline);
+	      if(comment_check == true) {
+		continue;
+	      }
+	      if(mainline_num != line_line) {
+		mainline_num++;
+	      } else if(mainline_num == line_line) {
+		line_line++;
+		mainline_num++;
+		char *main_search = strstr(mainline, "func:main");
+		if(main_search != NULL) {
+		  int the_line = mainline_num;
+		  int line_yo = 0;
+		  FILE *main_run = fopen(fname, "r+");
+		  char lineline[256];
+		  int ifnum = 0;
+		  bool is_in_if = false;
+		  int ifnum_ln = 0;
+		  int callnum = 0;
 
-						int ifnum = 0;
-						bool is_in_if = false;
-						int ifnum_ln = 0;
+		  while(fgets(lineline, sizeof(lineline), main_run) != NULL) {
 
-						int callnum = 0;
+		    /*if(ifscope == there) {
+		      char *b = strstr(lineline, "end");
+		      if(b != NULL) {
+		      ifscope = NULL;
+		      }
+		      continue;
+		      }*/
 
-						while(fgets(lineline, sizeof(lineline), main_run) != NULL) {
+		    comment_check = __check_com__(lineline);
+		    if(comment_check == true) {
+		      continue;
+		    }
 
-							/*if(ifscope == there) {
-								char *b = strstr(lineline, "end");
-								if(b != NULL) {
-									ifscope = NULL;
-								}
-								continue;
-							}*/
+		    if(line_yo != the_line) {
+		      line_yo++;
+		      continue;
+		    } else if(line_yo == the_line) {
+		      line_yo++;
+		      the_line++;
 
-							comment_check = __check_com__(lineline);
-							if(comment_check == true) {
-								continue;
-							}
+		      char *main_search = strstr(lineline, "func:main");
+		      char *end_of_main = strstr(lineline, "}");
 
-							if(line_yo != the_line) {
-								line_yo++;
-								continue;
-							} else if(line_yo == the_line) {
-								line_yo++;
-								the_line++;
-
-								char *main_search = strstr(lineline, "func:main");
-								char *end_of_main = strstr(lineline, "}");
-
-								if(end_of_main != NULL && main_search == NULL) {
-									break;	
-								} else if(end_of_main == NULL && main_search == NULL) {
-									FILE *out;
-									char *iffind = strstr(lineline, "~if");
-									if(iffind != NULL) {
-										ifnum++;
-										ifnum_ln = line_yo;
-										FILE *if_ = fopen(fname, "r+");
-										is_in_if = IS_IN_IF(if_, line_yo, fname);
-									}
-									char *callfind = strstr(lineline, "call~");
-									if(callfind != NULL) {callnum++;}
-									asm_interp_func_funcs(lineline,
-														  out,
-														  line_yo,
-														  ifnum_ln,
-														  fname,
-														  "main",
-														  ifnum,
-														  callnum,
-														  is_in_if);
-								}
-
-							}
-						}
-					} else if(main_search == NULL) {
-						continue;
-					}
-				}
-				if(mainline == NULL) {
-					break;
-				}
+		      if(end_of_main != NULL && main_search == NULL) {
+			break;	
+		      } else if(end_of_main == NULL && main_search == NULL) {
+			FILE *out;
+			char *iffind = strstr(lineline, "~if");
+			if(iffind != NULL) {
+			  ifnum++;
+			  ifnum_ln = line_yo;
+			  FILE *if_ = fopen(fname, "r+");
+			  is_in_if = IS_IN_IF(if_, line_yo, fname);
 			}
-			fclose(main_func);
-
-		} else if(func == NULL && main == NULL) {
-			continue;
+			char *callfind = strstr(lineline, "call~");
+			if(callfind != NULL) {callnum++;}
+			asm_interp_func_funcs(lineline,
+					      out,
+					      line_yo,
+					      ifnum_ln,
+					      fname,
+					      "main",
+					      ifnum,
+					      callnum,
+					      is_in_if);
+		      }
+		      
+		    }
+		  }
+		} else if(main_search == NULL) {
+		  continue;
 		}
-		line_num++;
-		if(line4 == NULL) {
-			break;
-		}
-
+	      }
+	      if(mainline == NULL) {
+		break;
+	      }
+	    }
+	    fclose(main_func);
+	    
+	  } else if(func == NULL && main == NULL) {
+	    continue;
+	  }
+	  line_num++;
+	  if(line4 == NULL) {
+	    break;
+	  }
+	  
 	}
 
 	/*fclose(file);*/
@@ -398,10 +397,27 @@ int asm_interp(int argc, char *argv[], bool INFO_DEBUG) {
 
 }
 
+void compile_help_arg() {
+
+  char *help = "WESM CLI HELP:\n\n"
+    "--help      | -h: Need help? Use -h\n"
+    "--output    | -o: Set name of executable output\n"
+    "--gcc       | -cc: Set your cflags (ex: wesm main.well -cc '-g -lpthread')\n"
+    "--assembly  | -a: Keep the assembly output file\n"
+    "--info      | -i: Shows extra debug information at compile time\n\n"
+    "--use-gnuld | -use-ld: Use gnu linker rather than gcc\n"
+    "--ldflags   | -ldflags: Set your gnu linker flags (ex: wesm main.well -use-ld -ldflags '-T link.ld')\n\n"
+    "--custom-format | -cf: Set nasm output format (ex: wesm main.well -cf elf32)\n"
+    "--use-yasm      | -yasm: Use yasm assembler rather than nasm\n";
+   printf("%s", help);
+}
+
 void compile(int argc, char *argv[]) {
 
 	struct ArgparseParser parser = argparse_init("wesm", argc, argv);
 
+	argparse_add_option(&parser, "--help", "-h", ARGPARSE_FLAG);
+	
 	argparse_add_option(&parser, "--output", "-o", ARGPARSE_FLAG);
 	argparse_add_option(&parser, "--assembly", "-a", ARGPARSE_FLAG); /* keeps the tmp assembly file*/
    	argparse_add_option(&parser, "--gcc", "-cc", ARGPARSE_FLAG); /* ex: wesm main.well -cc ::-lpthread -lcurl -g:: -o main*/
@@ -450,6 +466,12 @@ void compile(int argc, char *argv[]) {
 	char format_buf_yasm[0x100];
 	char gcc_buf[0x100];
 	char final_buf[0x100];
+
+	if(argparse_option_exists(parser, "--help") != ARGPARSE_NOT_FOUND ||
+	   argparse_option_exists(parser, "-h") != ARGPARSE_NOT_FOUND) {
+	  compile_help_arg();
+	  exit(0);
+	}
 
     	if(argparse_option_exists(parser, "--output") != ARGPARSE_NOT_FOUND ||
     		argparse_option_exists(parser, "-o") != ARGPARSE_NOT_FOUND) {
---
+++ src/asm_loops.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
